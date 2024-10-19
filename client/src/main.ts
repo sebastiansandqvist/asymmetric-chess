98118b9fe60c1abc4279f3906b3290478098da26
Sebastian Sandqvist <seb@sparque.me>
2024-10-19 08:35:29 -0700

    progress

--- client/src/main.ts
+++ client/src/main.ts
@@ -9,13 +9,53 @@ function draw() {
   resizeCanvas();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // TODO: should events go in a different loop?
+  handleClicks();
+
   drawBoard();
+  // highlightHoveredTile();
+  drawCircleInHoveredTile();
   drawPieces();
+  drawPieceSelectorMenus();
 
   requestAnimationFrame(draw);
 }
 
 draw();
+
+function handleClicks() {
+  const x = state.mouse.clickX;
+  const y = state.mouse.clickY;
+  if (x === -1 || y === -1) return;
+  if (state.status !== 'configuring') return; // TODO: eventually, move pieces
+
+  state.mouse.clickX = -1;
+  state.mouse.clickY = -1;
+
+  // TODO: handle if clicking in a menu
+  if (state.pieceSelector.dark.isOpen) {
+    return (state.pieceSelector.dark.isOpen = false);
+  }
+
+  if (state.pieceSelector.light.isOpen) {
+    return (state.pieceSelector.light.isOpen = false);
+  }
+
+  const { tileSize } = getRect();
+  const file = Math.floor(x / tileSize);
+  const rank = Math.floor(y / tileSize);
+
+  if (rank < 3) {
+    state.pieceSelector.dark.isOpen = true;
+    state.pieceSelector.dark.originFile = file;
+    state.pieceSelector.dark.originRank = rank;
+  }
+  if (rank > 4) {
+    state.pieceSelector.light.isOpen = true;
+    state.pieceSelector.light.originFile = file;
+    state.pieceSelector.light.originRank = rank;
+  }
+}
 
 function resizeCanvas() {
   canvas.width = window.innerWidth * devicePixelRatio;
@@ -51,3 +91,46 @@ function drawPieces() {
     ctx.drawImage(pieceSvg[piece.color][piece.type], x, y, tileSize, tileSize);
   }
 }
+
+function drawCircleInHoveredTile() {
+  const { tileSize } = getRect();
+
+  const radius = 20;
+  const validRanks = [0, 1, 2, 5, 6, 7];
+  const hoveredFile = Math.floor(state.mouse.x / tileSize);
+  const hoveredRank = Math.floor(state.mouse.y / tileSize);
+
+  if (!validRanks.includes(hoveredRank)) {
+    canvas.style.cursor = 'default';
+    return;
+  }
+
+  const hasPiece = state.board.some((piece) => piece.file === hoveredFile && piece.rank === hoveredRank);
+  if (hasPiece) {
+    canvas.style.cursor = 'alias';
+    return;
+  }
+
+  const centerX = hoveredFile * tileSize + tileSize / 2;
+  const centerY = hoveredRank * tileSize + tileSize / 2;
+
+  ctx.fillStyle = hoveredRank <= 2 ? 'rgba(0, 0, 0, 0.3)' : 'rgba(255, 255, 255, 0.6)';
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+  ctx.fill();
+
+  canvas.style.cursor = 'pointer';
+}
+
+function highlightHoveredTile() {
+  const { tileSize } = getRect();
+  const hoveredFile = Math.floor(state.mouse.x / tileSize);
+  const hoveredRank = Math.floor(state.mouse.y / tileSize);
+  resizeCanvas();
+  drawBoard();
+  drawPieces();
+  ctx.fillStyle = 'rgba(255, 255, 0, 0.5)'; // yellowish
+  ctx.fillRect(hoveredFile * tileSize, hoveredRank * tileSize, tileSize, tileSize);
+}
+
+function drawPieceSelectorMenus() {}
--- client/src/state.ts
+++ client/src/state.ts
@@ -1,10 +1,30 @@
 type Color = 'dark' | 'light';
 type Piece = 'pawn' | 'knight' | 'king' | 'rook' | 'queen' | 'bishop';
+type Status = 'configuring' | 'playing';
 
 export const state = {
+  status: 'configuring' as Status,
+  mouse: {
+    x: -1,
+    y: -1,
+    clickX: -1,
+    clickY: -1,
+  },
   budget: {
     dark: 39,
     light: 39,
+  },
+  pieceSelector: {
+    dark: {
+      isOpen: false,
+      originRank: -1,
+      originFile: -1,
+    },
+    light: {
+      isOpen: false,
+      originRank: -1,
+      originFile: -1,
+    },
   },
   board: [
     {
@@ -21,3 +41,18 @@ export const state = {
     },
   ],
 };
+
+document.addEventListener('mouseout', () => {
+  state.mouse.x = -1;
+  state.mouse.y = -1;
+});
+
+window.addEventListener('mousemove', (event) => {
+  state.mouse.x = event.clientX;
+  state.mouse.y = event.clientY;
+});
+
+window.addEventListener('mousedown', (event) => {
+  state.mouse.clickX = event.clientX;
+  state.mouse.clickY = event.clientY;
+});
